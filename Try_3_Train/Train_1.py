88bd63f98fb183667ecb3a4bb50ef18fec6b2e1f
Geoffrey-Fablab <geoffreytship3@gmail.com>
2024-10-05 14:20:14 +0100

    Entrainement avec mon dataset Running-Model

--- Try_3_Train/Train_1.py
+++ Try_3_Train/Train_1.py
@@ -3,25 +3,24 @@ But : Nous allons entrainer notre modèle YOLOv8 et exporter ce modèle
 
 Évaluation 1 : training avec un dataset contenant des images de guns, ketch...
 
+Evalution -1 : Nous voici presque à la fin de notre programme, maintenant, nous entrainons notre modèle
+                avec un dataset qu'on a rassemblé nous meme en prenant certains données au sain de la faculté
+                polytechnique.
+
 """
 
 from ultralytics import YOLO
 import os
 
 if __name__ == '__main__':
-    model = YOLO("yolov8s.pt")
+    model = YOLO("runs/detect/train3/weights/last.pt")
 
-    results = model.train(
-        data="data.yaml",
-        epochs=100,
-        patience=25,
-        batch=-1,
-        imgsz=640,
-        cache=True,
-        device=0,
-        lr0=0.0001,
-        lrf=0.5,
-        plots=True
-    )
+    # Resume training
+    results = model.train(resume=True)
+
+    # results = model.train(data="data.yaml", epochs=100, patience=25, batch=-1, imgsz=640, cache=True, device=0, lr0=0.0001, lrf=0.1, plots=True)
+
+
+
 
 
