2c33ab3db4c3eac68156104c170041eb954b3bae
Geoffrey-Fablab <geoffreytship3@gmail.com>
2024-10-07 18:17:49 +0100

    Ajout du niveau de confiance mise à 0.7 et du son lors de la détection des personne, au dessus de cela on a ajouté la possibilité d'arreter le son

--- Try_2_OK/Interface_graphique.py
+++ Try_2_OK/Interface_graphique.py
@@ -1,11 +1,22 @@
 import cv2
 from ultralytics import YOLO
+from screeninfo import get_monitors
+import pygame
+
 
 # Charger le modèle YOLOv8
-model = YOLO('yolov8n.pt')  # Vous pouvez choisir un autre modèle YOLOv8 selon vos besoins
+model = YOLO('yolov8m.pt')  # Vous pouvez choisir un autre modèle YOLOv8 selon vos besoins
+
+CONFIDENCE_THRESHOLD = 0.7  # C'est prévue une conf = 0,8
 
 # Remplacez 'rtsp://username:password@ip_address:port/stream' par l'URL RTSP de votre caméra
 rtsp_url = 'rtsp://admin:Burotop01@10.10.50.127:554/Streaming/Channels/101'
+
+# Initialiser pygame pour l'audio
+pygame.mixer.init()
+
+# Charger le fichier audio
+sound = pygame.mixer.Sound('son_Alarm.wav')
 
 # Créez un objet VideoCapture
 cap = cv2.VideoCapture(rtsp_url)
@@ -26,11 +37,19 @@ while cap.isOpened():
     # Effectuer la détection sur l'image
     results = model(frame)
 
+    person_detected = False  # Indicateur pour la détection de personnes
+
     # Filtrer uniquement les personnes (class_id = 0 pour la classe 'person' dans les modèles YOLO)
     for result in results:
         boxes = result.boxes  # Obtenir les boîtes englobantes
         for box in boxes:
             class_id = int(box.cls)
+
+            # Supprimer les détections en dessous du seuil CONFIDENCE_THRESHOLD
+            if float(box.conf) < CONFIDENCE_THRESHOLD:
+                continue
+
+            # Détection que de la classe personne
             if class_id == 0:  # 0 correspond à la classe 'person'
                 # Récupérer les coordonnées de la boîte englobante
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -38,8 +57,14 @@ while cap.isOpened():
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, 'Person', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
+            # Définir que la personne est détectée
+            person_detected = True
+
+    if person_detected:
+        sound.play()
+
     # Redimensionner l'image (par exemple, à 50% de la taille originale)
-    scale_percent = 70  # Pourcentage de redimensionnement
+    scale_percent = 50  # Pourcentage de redimensionnement
     width = int(frame.shape[1] * scale_percent / 100)
     height = int(frame.shape[0] * scale_percent / 100)
     resized_frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
@@ -49,8 +74,12 @@ while cap.isOpened():
     cv2.imshow(window_name, resized_frame)
 
     # Obtenir les dimensions de l'écran
-    screen_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    screen_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    # screen_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    # screen_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    # Obtenir les dimensions de l'écran principal
+    screen = get_monitors()[0]
+    screen_width, screen_height = screen.width, screen.height
 
     # Calculer la position pour centrer la fenêtre
     x = int((screen_width - width) / 2)
@@ -59,9 +88,12 @@ while cap.isOpened():
     # Placer la fenêtre au centre de l'écran
     cv2.moveWindow(window_name, x, y)
 
-    # Sortir de la boucle avec la touche 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Vérifier les entrées clavier
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):  # Quitter avec la touche 'q'
         break
+    elif key == ord(' '):  # Appuyer sur la touche espace pour couper/activer le son
+        pygame.mixer.stop()  # Arrêter tous les sons
 
 # Libérer l'objet VideoCapture et fermer les fenêtres
 cap.release()
