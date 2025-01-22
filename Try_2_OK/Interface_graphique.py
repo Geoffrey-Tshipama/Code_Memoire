21130810b45eb84ee56e7748e732f2eb0da70a42
Geoffrey-Fablab <geoffreytship3@gmail.com>
2025-01-22 16:48:39 +0100

    Vérification de l'exécution final du code

--- Try_2_OK/Interface_graphique.py
+++ Try_2_OK/Interface_graphique.py
@@ -9,8 +9,8 @@ import Recording
 model = YOLO('best_5.pt')
 
 CONFIDENCE_THRESHOLD = 0.5  # Le seuil de détection
-# EXCLUDED_CLASS_ID = 0
-# L'exclusion de la classe 0 qui détecte de personne non violente
+# EXCLUDED_CLASS_ID = 3
+# L'exclusion de la classe 3 qui détecte de personne non violente
 
 # L'URL RTSP de ma caméra
 rtsp_url = 'rtsp://admin:Burotop01@10.10.50.122:554/Streaming/Channels/101'
@@ -24,12 +24,13 @@ pygame.mixer.music.load('son_Alarm.wav')
 # Récupération des noms des classes
 class_names = model.names
 # Cela donne un dictionnaire avec les IDs comme clés et les noms comme valeurs
-# print(class_names)
 
 # Créez un objet VideoCapture
 cap = cv2.VideoCapture(rtsp_url)
 
-tracker = DeepSort(max_age=20)
+tracker = DeepSort(max_age=30)
+# max_age est le nombre maximum des frames de tenir une prédiction avant de le faire disparaitre
+
 
 # Vérifiez si la connexion est ouverte
 if not cap.isOpened():
@@ -51,20 +52,17 @@ while cap.isOpened():
         print("Erreur : Impossible de lire le flux vidéo")
         break
 
-    #    detected = False
-
     # Effectuer la détection sur l'image
     detections = model(frame)[0]
 
     results = []
 
-    # Filtrer uniquement les personnes (class_id = 0 pour la classe 'person' dans les modèles YOLO)
     for data in detections.boxes.data.tolist():
         # La probabilité de chaque détection
         confidence = data[4]
 
         # Récupérez l'id du nom de la classe
-        # class_id = int(data[5])
+        class_id = int(data[5])
 
         # Supprimer les détections en dessous du seuil CONFIDENCE_THRESHOLD
         if float(confidence) < CONFIDENCE_THRESHOLD:
@@ -75,11 +73,6 @@ while cap.isOpened():
 
         # Récupérer les coordonnées de la boîte englobante
         xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
-        class_id = int(data[5])
-
-        # Dessiner la boîte englobante et l'étiquette
-        # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        # cv2.putText(frame, 'Person', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         # ajouter le cadre de délimitation (x, y, l, h), la confiance et l'identifiant de classe à la liste des
         # résultats
@@ -98,16 +91,15 @@ while cap.isOpened():
         xmin, ymin, xmax, ymax = int(ltrb[0]), int(
             ltrb[1]), int(ltrb[2]), int(ltrb[3])
 
-        class_id = track.get_det_class()  # Get the class ID from the track
-        class_name = class_names[class_id]  # Get the class name from the class ID
+        class_id = track.get_det_class()  # Obtenir l'ID de la classe depuis la piste.
+        class_name = class_names[class_id]  # Récupérez le nom de la classe
 
         # Jouer un son si la classe "violence" est détectée
         # if class_name.lower() != "NonViolence":
         # detected = True
         # pygame.mixer.music.play()
 
-        # draw the bounding box and the track id
-        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+        # dessiner la boite englobante, mettre id du track et le nom de la classe au dessus de la boite
         # Draw the class name above the bounding box
         cv2.putText(frame, f"{class_name} ({track_id})", (xmin, ymin - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
